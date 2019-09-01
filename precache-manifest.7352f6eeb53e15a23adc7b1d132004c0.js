4b2deea1faf38f4b985c6bbbb811022b9123040c
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-09-02 00:01:41 +0200

    Updates

---
+++ precache-manifest.7352f6eeb53e15a23adc7b1d132004c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d690457c986f18e98675ba77ee405b",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "d0f7ebf19e52d03d2297",
+    "url": "/shop/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4cf52097a47553ee28e3",
+    "url": "/shop/static/css/main.4c6dd807.chunk.css"
+  },
+  {
+    "revision": "d0f7ebf19e52d03d2297",
+    "url": "/shop/static/js/2.6cbc14dc.chunk.js"
+  },
+  {
+    "revision": "4cf52097a47553ee28e3",
+    "url": "/shop/static/js/main.40c71840.chunk.js"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  }
+]);
